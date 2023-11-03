a37b86b557010bde459c402ddc25e096d8436136
김범수 <phsyobs@naver.com>
2023-11-03 16:38:28 +0900

    awf

--- 버전1_도전과제/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('A/', views.problem_a),
-    path('B/', views.problem_b),
-    path('C/', views.problem_c),
-    path('D/', views.problem_d),
-]
